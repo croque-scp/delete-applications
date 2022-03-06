1bea9b04f8103f2c3d40935f3d8b6c9840ee46bb
Ross Williams <rossjrw@gmail.com>
2022-03-06 17:15:48 +0000

    Increment version

--- delete-applications.user.js
+++ delete-applications.user.js
@@ -7,7 +7,7 @@ For installation instructions, see https://scpwiki.com/usertools
 // ==UserScript==
 // @name        Wikidot applications deleter
 // @description Adds a button to delete applications from your Wikidot inbox.
-// @version     v1.0.0
+// @version     v1.0.1
 // @updateURL   https://github.com/croque-scp/delete-applications/raw/main/delete-applications.user.js
 // @downloadURL https://github.com/croque-scp/delete-applications/raw/main/delete-applications.user.js
 // @include     https://www.wikidot.com/account/messages*
